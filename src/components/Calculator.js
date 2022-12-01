4c77e56f9cde31ded613888f22a30226380bb7c0
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-12-02 01:20:28 +0530

    Create Calculator.js

---
+++ src/components/Calculator.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="calculator-div">
+        <div className="result">0</div>
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+        <button type="button" className="highlight">÷</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button" className="highlight">x</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button" className="highlight">-</button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button" className="highlight">+</button>
+        <button type="button" className="zero">0</button>
+        <button type="button">.</button>
+        <button type="button" className="highlight">=</button>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
