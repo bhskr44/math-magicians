192d08c577a30360b174d222be4f4d1dffac1df0
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-12-02 02:21:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import React from 'react';
 import Calculator from './components/Calculator';
-import react from 'react';
-function App() {
-  return (
-    <div className="calculator">
-      <Calculator />
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
